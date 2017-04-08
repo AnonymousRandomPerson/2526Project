0b1a4025a582ea8164da40bc2a45fbbd843acd70
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2017-04-08 00:39:47 -0400

    Initial application

---
+++ gui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class Gui:
+
+    """The main GUI screen."""
+
+    def __init__(self):
+        """Initializes the screen elements."""
+
+        self.root = Tk()
+        self.root.wm_title("2526 Project")
+
+        self.root.mainloop()
